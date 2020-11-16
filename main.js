45b016b15fa7e74451a822f90e90fa045fd27faa
Matthew Robinson <mattbze@yahoo.com>
2020-11-16 16:11:15 -0600

    again

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let button = document.querySelector("button");
 
 const consolidate = (whatever) =>{
     let output = {
-       // title: whatever.consolidated_weather[0]
+
         state : whatever.consolidated_weather[0].weather_state_name,
         abbr : whatever.consolidated_weather[0].weather_state_abbr
         
@@ -18,12 +18,13 @@ const consolidate = (whatever) =>{
 button.addEventListener("click", () => {
     let city = document.querySelector("#city").value;
     getWeatherByCity(city,(weatherData)=>{
+        
         makeWeatherCard(consolidate(weatherData))
     })
 })
 
 document.body.append(makeElement("card", "this is a new card" ))
-
+`https://www.metaweather.com/static/img/weather/ico/${data[0]}.ico`;
 
 
 // let card = makeCard(
--- make-element.js
+++ make-element.js
@@ -1,14 +1,18 @@
 
 
-export const makeElement = (name, inner,display ) =>{
-    let el = document.createElement('div')
+export const makeElement = (tag,name, inner,display ) =>{
+    let el = document.createElement(tag)
     el.className = name
     el.innerHTML = inner
     el.style.display = display
     return el
+
 }
 
 
 export const doClose = function (btn) {
     btn.currentTarget.parentNode.remove()
 }
+
+
+    
