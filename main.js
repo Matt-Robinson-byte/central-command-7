670c08fcc69cf098be3115dc850370ff46874c40
starskreen619 <72418975+starskreen619@users.noreply.github.com>
2020-11-18 00:59:07 -0500

    css and html updates

--- main.js
+++ main.js
@@ -1,51 +1,40 @@
-
-import {makeWeatherCard} from "./makeCard.js"
-import {getWeatherByCity} from "./getWeather.js"
+import { makeWeatherCard } from "./makeCard.js";
+import { getWeatherByCity } from "./getWeather.js";
 import { getPictureByCity } from "./getCityPic.js";
-import {consolidate} from "./importantDataFunction.js"
-import {getBrew} from "./brew.js"
+import { consolidate } from "./importantDataFunction.js";
+import { getBrew } from "./brew.js";
 // import {makePictureCard} from "./addCityImage.js"
 // import {getPic} from "./backgroundPic.js"
 // import {makeElement} from "./make-element.js"
 
 let button = document.querySelector("button");
 
-
 button.addEventListener("click", () => {
-    let city = document.querySelector("#city").value;
-    //this could be a function
-    let card = document.createElement('div')
-    card.classList.add('card')
-    document.body.append(card)
+  let city = document.querySelector("#city").value;
+  //this could be a function
+  let card = document.createElement("div");
+  card.classList.add("card");
+  document.body.append(card);
 
+  let breweryCard = document.createElement("div");
+  breweryCard.classList.add("breweryCard");
+  document.body.append(breweryCard);
 
-    let breweryCard = document.createElement('div')
-    breweryCard.classList.add('breweryCard')
-    document.body.append(breweryCard)
+  getWeatherByCity(city, (weatherData) => {
+    console.log(weatherData);
+    card.append(makeWeatherCard(consolidate(weatherData)));
+    // card.append(makeWeatherCard(consolidate(weatherData)))
+  });
+  getPictureByCity(city, (data) => {
+    card.style.backgroundImage = `url(${data.photos[0].image.web})`;
+  });
 
+  getBrew(city, (breweries) => {
+    breweryCard.innerHTML.append(breweries.name);
+  });
+  // getPictureByCity(city, cityData=>{
+  //     console.log(cityData)
+  // })
 
-    getWeatherByCity(city,(weatherData)=>{
-        console.log (weatherData)
-        card.append(makeWeatherCard(consolidate(weatherData)))
-        // card.append(makeWeatherCard(consolidate(weatherData)))
-        
-    })
-    getPictureByCity(city, (data)=>{
-        card.style.backgroundImage = `url(${data.photos[0].image.web})`
-    })
-
-    
-    getBrew(city,(breweries) =>{
-        breweryCard.innerHTML.append(breweries.name)
-    })
-    // getPictureByCity(city, cityData=>{
-    //     console.log(cityData)
-    // })
-
-    getBrew(city)
-    
-    
-     
-   
+  getBrew(city);
 });
-
